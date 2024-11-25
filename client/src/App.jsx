c12d5d587627518aad24fda98a68718dcd95208b
sukiariska <sukiariska8@gmail.com>
2024-11-25 12:06:39 +0700

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,38 +1,21 @@
-import { ChakraProvider } from '@chakra-ui/react'
-
+import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import Home from "./pages/home";
-
+import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
-
 import SignUp from "./pages/SignUp";
-import { system } from '@chakra-ui/react/preset';
-
-
-
+import { system } from "@chakra-ui/react/preset";
 
 
 export default function App() {
-
   return (
-  <ChakraProvider value ={system}>
-    
+  <ChakraProvider value={system}>
     <BrowserRouter>
-
       <Routes>
-
-        <Route path='/home' element={<Home />} />
-
+        <Route path='/' element={<Home />} />
         <Route path='/signin' element={<SignIn />} />
-
         <Route path='/signup' element={<SignUp />} />
-
       </Routes>
-
     </BrowserRouter>
-    </ChakraProvider>
-
-);
-
+  </ChakraProvider>
+  );
 }
